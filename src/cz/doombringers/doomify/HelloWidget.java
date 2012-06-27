4a58258da6ee92501b6922c43f520904ae973d26
Jan Zalsky <zalsky@computerhelp.cz>
2012-06-28 00:24:00 +0200

    widget refresh working :) hurray

--- src/cz/doombringers/doomify/HelloWidget.java
+++ src/cz/doombringers/doomify/HelloWidget.java
@@ -1,5 +1,72 @@
 package cz.doombringers.doomify;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+
+import android.widget.RemoteViews;
+
 public class HelloWidget extends AppWidgetProvider {
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+
+		Timer timer = new Timer();
+		timer.scheduleAtFixedRate(new MyTime(context, appWidgetManager), 1,
+				1000);
+
+		/*
+		 * Intent intent = new Intent(context, UpdateService.class);
+		 * context.startService(intent);
+		 */
+	}
+
+	private class MyTime extends TimerTask {
+		RemoteViews remoteViews;
+		AppWidgetManager appWidgetManager;
+		ComponentName thisWidget;
+		DateFormat format = SimpleDateFormat.getTimeInstance(
+				SimpleDateFormat.MEDIUM, Locale.getDefault());
+
+		public MyTime(Context context, AppWidgetManager appWidgetManager) {
+			this.appWidgetManager = appWidgetManager;
+			remoteViews = new RemoteViews(context.getPackageName(),
+					R.layout.widget);
+			thisWidget = new ComponentName(context, HelloWidget.class);
+		}
+
+		@Override
+		public void run() {
+			remoteViews.setTextViewText(R.id.widget_textview, "Time = "
+					+ format.format(new Date()));
+			appWidgetManager.updateAppWidget(thisWidget, remoteViews);
+		}
+	}
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// v1.5 fix that doesn't call onDelete Action
+		final String action = intent.getAction();
+		if (AppWidgetManager.ACTION_APPWIDGET_DELETED.equals(action)) {
+			final int appWidgetId = intent.getExtras().getInt(
+					AppWidgetManager.EXTRA_APPWIDGET_ID,
+					AppWidgetManager.INVALID_APPWIDGET_ID);
+			if (appWidgetId != AppWidgetManager.INVALID_APPWIDGET_ID) {
+				this.onDeleted(context, new int[] { appWidgetId });
+			}
+		} else {
+			super.onReceive(context, intent);
+		}
+	}
+
 }
