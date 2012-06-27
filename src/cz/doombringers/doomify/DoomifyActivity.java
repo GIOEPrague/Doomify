eead4d13a9f29d73e7bee7fa6af0d0889c0e31ac
Jan Zalsky <zalsky@computerhelp.cz>
2012-06-27 23:02:06 +0200

    init

---
+++ src/cz/doombringers/doomify/DoomifyActivity.java
@@ -0,0 +1,13 @@
+package cz.doombringers.doomify;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DoomifyActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
